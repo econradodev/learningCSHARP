28c8de079d1f856d09757623691794ffe1bc80d0
econradodev <econradodev@gmail.com>
2024-05-28 11:36:44 -0300

    conta bancaria nivel iniciante

---
+++ ContaBancariaBeginner/ContaBancariaBeginner/Conta.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancariaBeginner
+{
+    internal class Conta
+    {
+        private int _numeroDaConta;
+        private string _nome;
+        public double _saldo;
+
+        public Conta() { }
+
+        public Conta(int numeroDaConta, string nome, double saldo)
+        {
+            _numeroDaConta = numeroDaConta;
+            _nome = nome;
+            _saldo = saldo;
+        }
+
+        public void AdicionarSaldo(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            _saldo -= valor + 5.00;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " 
+                + _numeroDaConta
+                + ", Titular: "
+                + _nome
+                + ", Saldo: $ "
+                + _saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ContaBancariaBeginner/ContaBancariaBeginner/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaBancariaBeginner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite o número da conta: ");
+            int NumeroDaConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome do titular da conta: ");
+            string Nome = Console.ReadLine();
+            double Saldo = 0;
+
+            Conta conta = new Conta(NumeroDaConta, Nome, Saldo);
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char Verificador = char.Parse(Console.ReadLine());
+
+            if (Verificador == 'n')
+            {
+                Console.WriteLine();
+                Console.WriteLine("Dados da conta:");
+                Console.WriteLine(conta);
+
+            } else if (Verificador == 's')
+            {
+                Console.Write("Digite o valor do depósito inicial: ");
+                conta._saldo = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+                Console.WriteLine("Dados da conta:");
+                Console.WriteLine(conta);
+
+            }
+
+            Console.WriteLine();
+            Console.Write("Digite um valor para depósito: ");
+            double Valor = double.Parse(Console.ReadLine());
+            conta.AdicionarSaldo(Valor);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Digite um valor para saque: ");
+            Valor = double.Parse(Console.ReadLine());
+            conta.Saque(Valor);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+
+
+
+        }
+    }
+}
